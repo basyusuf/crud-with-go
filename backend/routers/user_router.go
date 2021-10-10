6931082c7d84a85637dd9916332f95eebea9063b
Yusuf Baş <y.penava@hotmail.com>
2021-10-11 02:41:48 +0300

    Edit documentation and edit create user method

--- backend/routers/user_router.go
+++ backend/routers/user_router.go
@@ -10,7 +10,7 @@ func UserRouters(router *mux.Router) {
 	routerPrefix := "/users"
 	router.HandleFunc(routerPrefix, controllers.GetAllUser).Methods("GET")
 	router.HandleFunc(routerPrefix+"/{id:[0-9]+}", controllers.GetUserById).Methods("GET")
-	router.HandleFunc(routerPrefix, controllers.CreateUser).Methods("POST")
+	router.HandleFunc(routerPrefix, controllers.CreateUser).Methods("PUT")
 	router.HandleFunc(routerPrefix+"/{id:[0-9]+}", controllers.UpdateUser).Methods("PATCH")
 	router.HandleFunc(routerPrefix+"/{id:[0-9]+}", controllers.DeleteUser).Methods("DELETE")
 }
